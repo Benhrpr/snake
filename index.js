8d0988598ff7dc2ea18257a32d631509caaeb7d2
Ben Harper <benharper2009@outlook.com>
2023-07-21 18:59:35 +1000

    asd

--- index.js
+++ index.js
@@ -144,10 +144,9 @@ function checkGameOver(){
     }
 };
 function displayGameOver(){
-    ctx.font = "50px MV Boli";
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
-    ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
+    ctx.fillText("You died...", gameWidth / 2, gameHeight / 2);
     running = false;
 };
 function resetGame(){
